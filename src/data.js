6d9b1d4dfe300bda87532f41d6d29d961fec6961
erdldncr <erdldncr@gmail.com>
2021-06-07 01:03:45 +0100

    template has been done

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+      {
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/p1.jpeg',
+        price: 60,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 10
+      },
+      {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/p2.jpeg',
+        price: 50,
+        brand: ' Nike',
+        rating: 4.2,
+        numReviews: 5
+      },
+      {
+        _id: '3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/p3.jpeg',
+        price: 70,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 8
+      }, {
+        _id: '4',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/p4.jpeg',
+        price: 70,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 8
+      },
+    ]
+  }
+  
